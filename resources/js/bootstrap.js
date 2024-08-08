c5afbac6f6c4520808cb133010be347ae05ec1c0
xarthaa <manager.arthachan@gmail.com>
2024-08-08 21:04:43 +0800

    fix: ws

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -28,5 +28,5 @@ window.Echo = new Echo({
     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
     forceTLS: false,
-    enabledTransports: ['ws', 'wss'],
+    enabledTransports: ['ws'],
 });
